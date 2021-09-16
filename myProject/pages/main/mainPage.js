0c7898b8785c77d9f6892f97df32368cffdbb0b8
MaryiaTatarevich <masha.tatarevich5@gmail.com>
2021-09-16 16:35:41 +0300

    other size

--- myProject/pages/main/mainPage.js
+++ myProject/pages/main/mainPage.js
@@ -1,8 +1,7 @@
 function mainPageLoading() {
     //для back очистить поле
-    let findGame = document.getElementById('allGame')
-    let findRecords = document.getElementById('allRecords')
-
+    let findGame = document.getElementById('allGame');
+    let findRecords = document.getElementById('allRecords');
     if (findGame) {
         findGame.remove()
     }else if(findRecords){
@@ -12,20 +11,18 @@ function mainPageLoading() {
         `<a href="#Game" onclick="switchToGamePage()"><span>New Game</span></a>
     <a href="#Records" onclick="switchToRecordsPage()"><span>Records</span></a>
     <div id ="musicIcon" onclick = 'music()'></div>`
-    
     return mainPage
+    
 }
 let playMusic = false
 let backgroundMusic = new Audio('./pages/game/sounds/backgroundMusic.mp3')
 function music(){
     let musicIcon = document.getElementById('musicIcon');
 if(!playMusic){
-    
     playMusic = true
     backgroundMusic.play()
     backgroundMusic.volume = 0.1;
-    console.log(musicIcon)
-     musicIcon.style.backgroundImage ='url(./pages/game/img/icon_music.png)'
+    musicIcon.style.backgroundImage ='url(./pages/game/img/icon_music.png)'
     
 }else{
     playMusic = false
